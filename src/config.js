22c813101a3ae39695aef8e789b218adfe64b9b7
Vladimir Grichina <vgrichina@gmail.com>
2019-03-27 01:08:32 -0700

    Add config.js to connect contract

---
+++ src/config.js
@@ -0,0 +1,7 @@
+
+const config = {
+    baseUrl: 'https://studio.nearprotocol.com/contract-api',
+    nodeUrl: 'https://studio.nearprotocol.com/devnet',
+    contractName: 'studio-2vd8wnacw'
+};
+Cookies.set('fiddleConfig', config);
