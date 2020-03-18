f058894194c5cea3e6b8b6c6a5bf28d56af68c3d
Willem Wyndham <willem@cs.umd.edu>
2020-03-18 13:41:58 -0400

    Use sdk and clean up code
    also add dockerfile and CI

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require("gulp");
-const nearUtils = require("near-bindgen-as/compiler");
+const nearUtils = require("near-sdk-as/compiler");
 
 function build_wasm(done){
   nearUtils.compile("./assembly/main.ts", "./out/main.wasm", done);
