6dbb04d698426e4097fcbeb23b6f3d75fd557338
Mike Purvis <mikedotexe@gmail.com>
2020-03-18 14:46:59 -0700

    rework tests and package.json

--- src/test.js
+++ src/test.js
@@ -2,25 +2,19 @@ describe("Authorizer", function() {
     let near;
     let contract;
     let alice;
-    let bob = "bob.near";
-    let eve = "eve.near";
-  
+
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
     // Common setup below
     beforeAll(async function() {
-      const config = await nearlib.dev.getConfig();
-      near = await nearlib.dev.connect();
-      alice = nearlib.dev.myAccountId;
-      const url = new URL(window.location.href);
-      config.contractName = url.searchParams.get("contractName");
-      console.log("nearConfig", config);
-      contract = await near.loadContract(config.contractName, {
+      near = await nearlib.connect(nearConfig);
+      alice = nearConfig.contractName;
+      contract = await near.loadContract(nearConfig.contractName, {
         // NOTE: This configuration only needed while NEAR is still in development
         // View methods are read only. They don't modify the state, but usually return some value. 
-        viewMethods: ["whoSaidHi"],
+        viewMethods: ["getCurrentGame", "getGame", "getRecentGames"],
         // Change methods can modify the state. But you don't receive the returned value when called.
-        changeMethods: ["sayHi"],
+        changeMethods: ["createOrJoinGame", "makeMove", "giveUpCurrentGame"],
         sender: alice
       });
     });
@@ -31,15 +25,12 @@ describe("Authorizer", function() {
         // There can be some common setup for each test.
       });
 
-      it("check, say hi, check again", async function() {
-        // Checking that no one said Hi before us.
-        const nobody = await contract.whoSaidHi();
-        expect(nobody).toBeFalsy();
-        // Saying Hi!
-        await contract.sayHi();
-        // Checking again
-        const shouldBeMe = await contract.whoSaidHi();
-        expect(shouldBeMe).toBe(alice);
+      it("creates a game that shows up as expected in recent games", async function() {
+        await contract.createOrJoinGame();
+        const recentGames = await contract.getRecentGames();
+        console.log("aloha recentGames", recentGames);
+        expect(recentGames.length).toBe(1);
+        expect(recentGames[0]['game']['player1']).toBe(nearConfig.contractName);
       });
-  });
+    });
 });
